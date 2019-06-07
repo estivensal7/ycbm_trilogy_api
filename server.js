7c02a5257292b2c0cb758d655fc34dc33b90ecaa
estivensal7 <estivensal7@gmail.com>
2019-06-07 00:54:20 -0400

    adding cors

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET,PUT,POST,DELETE,OPTIONS"
+	);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+	);
+	next();
+});
+
 // Route to serve JSON data
 app.use("/", function(req, res) {
 	return res.json().status(200);
